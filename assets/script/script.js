a102a0cb1f854c56eb866d017c501d4531e4ebe7
ashish sood <ashishsood1998@gmail.com>
2026-04-15 12:33:16 -0500

    functionn created for browser info

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,4 +15,17 @@ function getCookie(name) {
   }
   return "";
 }
-getcookies();
+
+function getBrowserName() {
+  let data = navigator.userAgent;
+
+  if (data.indexOf("Chrome") !== -1) {
+    return "Chrome";
+  } else if (data.indexOf("Firefox") !== -1) {
+    return "Firefox";
+  } else if (data.indexOf("Safari") !== -1) {
+    return "Safari";
+  } else {
+    return "Unknown Browser";
+  }
+}
