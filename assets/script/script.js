c7dbf675c538a86131238bbf7bdc771f8739fdc8
ashish sood <ashishsood1998@gmail.com>
2026-04-15 11:26:42 -0500

    function created to get cookies

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,16 @@
 function setCookie(name, value, seconds) {
   document.cookie = name + "=" + value + ";max-age=" + seconds + ";path=/";
 }
+
+function getCookie(name) {
+  let cookies = document.cookie.split(";");
+  for (let i = 0; i < cookies.length; i++) {
+    let cook = cookies[i].trim();
+
+    if (cook.indexOf(name + "=") === 0) {
+      return cook.substring(name.length + 1);
+    }
+  }
+  return "";
+}
+getcookies();
